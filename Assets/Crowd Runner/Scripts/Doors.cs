96684d65e18f5aaab52be59db01c8814203b3ef2
nicolajhansen97 <nicolajhansen97@live.dk>
2025-07-27 13:38:34 +0200

    Added door logic
    Added doors and logic. Also fixed the bug that the camera followed the player.

---
+++ Assets/Crowd Runner/Scripts/Doors.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Doors : MonoBehaviour
+{
+    enum BonusType { Addition, Difference, Product, Division }
+
+    [Header(" Elements ")]
+    [SerializeField] private SpriteRenderer rightDoorRenderer;
+    [SerializeField] private SpriteRenderer leftDoorRenderer;
+    [SerializeField] private TextMeshPro rightDoorText;
+    [SerializeField] private TextMeshPro LeftDoorText;
+
+    [Header(" Settings ")]
+    [SerializeField] private BonusType rightDoorBonusType;
+    [SerializeField] private int rightDoorBonusAmount;
+    [SerializeField] private BonusType LeftDoorBonusType;
+    [SerializeField] private int leftDoorBonusAmount;
+    [SerializeField] private Color bonusColor;
+    [SerializeField] private Color penaltyColor;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConfigureDoors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ConfigureDoors()
+    {
+
+        //Right
+
+        switch(rightDoorBonusType)
+        {
+            case BonusType.Addition:
+
+                rightDoorRenderer.color = bonusColor;
+                rightDoorText.text = $"+{rightDoorBonusAmount}";
+                break;
+
+            case BonusType.Difference:
+                rightDoorRenderer.color = penaltyColor;
+                rightDoorText.text = $"-{rightDoorBonusAmount}";
+                break;
+
+            case BonusType.Product:
+                rightDoorRenderer.color = bonusColor;
+                rightDoorText.text = $"x{rightDoorBonusAmount}";
+                break;
+
+            case BonusType.Division:
+                rightDoorRenderer.color = penaltyColor;
+                rightDoorText.text = $"/{rightDoorBonusAmount}";
+                break;
+        }
+
+
+        //Left
+        switch (LeftDoorBonusType)
+        {
+            case BonusType.Addition:
+
+                leftDoorRenderer.color = bonusColor;
+                LeftDoorText.text = $"+{leftDoorBonusAmount}";
+                break;
+
+            case BonusType.Difference:
+                leftDoorRenderer.color = penaltyColor;
+                LeftDoorText.text = $"-{leftDoorBonusAmount}";
+                break;
+
+            case BonusType.Product:
+                leftDoorRenderer.color = bonusColor;
+                LeftDoorText.text = $"x{leftDoorBonusAmount}";
+                break;
+
+            case BonusType.Division:
+                leftDoorRenderer.color = penaltyColor;
+                LeftDoorText.text = $"/{leftDoorBonusAmount}";
+                break;
+        }
+    }
+}
